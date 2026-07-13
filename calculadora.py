dc1159a521bf45daa4f71e4927e43a5cdb476d30
jhon palomino <jhonpalominoroque654@gmail.com>
2026-07-13 18:42:11 -0500

    agregar operaciones basicas a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,21 @@
 numero_1 = int(input("ingrese primer valor:"))
+operador = input("ingrese el operador que dese")
 numero_2 = int(input("ingrese segundo valor:"))
-resultado = numero_1 + numero_2
-print("primer valor:", numero_1)
-print("segundo valor:", numero_2)
-print("resultado: ", resultado)
+if operador == "+":
+    print("resultado: ", numero_1 + numero_2)
+elif operador == "-":
+    print("resultado: ", numero_1 - numero_2)
+elif operador == "*":
+    print("resultado: ", numero_1 * numero_2)
+elif operador == "/":
+    print("resultado: ", numero_1 / numero_2)
+elif operador == "//":
+    print("resultado: ", numero_1 // numero_2)
+elif operador == "**":
+    print("resultado: ", numero_1 ** numero_2)
+elif operador == "%":
+    print("resultado: ", numero_1 % numero_2)
+    
+else:
+    print("valor invalido o operador faltante espere futuras actualizaciones")  
 
