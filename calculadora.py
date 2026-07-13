cd5485d3204f8bbb62e208d3eb42d06f3efad97e
jhon palomino <jhonpalominoroque654@gmail.com>
2026-07-12 19:09:02 -0500

    version 1.0: calculadora con suma

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("ingrese primer valor:"))
+numero_2 = int(input("ingrese segundo valor:"))
+resultado = numero_1 + numero_2
+print("primer valor:", numero_1)
+print("segundo valor:", numero_2)
+print("resultado: ", resultado)
+
