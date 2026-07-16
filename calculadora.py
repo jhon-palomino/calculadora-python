a0aabc7262758ebff79e1b0e1b24af2b1a78b8a2
jhon palomino <jhonpalominoroque654@gmail.com>
2026-07-16 07:31:41 -0500

    version 3.0: agregar ciclo while y validacion de division entre cero

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,31 @@
-numero_1 = int(input("ingrese primer valor:"))
-operador = input("ingrese el operador que dese")
-numero_2 = int(input("ingrese segundo valor:"))
-if operador == "+":
-    print("resultado: ", numero_1 + numero_2)
-elif operador == "-":
-    print("resultado: ", numero_1 - numero_2)
-elif operador == "*":
-    print("resultado: ", numero_1 * numero_2)
-elif operador == "/":
-    print("resultado: ", numero_1 / numero_2)
-elif operador == "//":
-    print("resultado: ", numero_1 // numero_2)
-elif operador == "**":
-    print("resultado: ", numero_1 ** numero_2)
-elif operador == "%":
-    print("resultado: ", numero_1 % numero_2)
+repetir = "s"
+while repetir == "s":
+    numero_1 = int(input("ingrese primer valor:"))
+    operador = input("ingrese el operador que dese")
+    numero_2 = int(input("ingrese segundo valor:"))
+    if operador == "+":
+        print("resultado: ", numero_1 + numero_2)
+    elif operador == "-":
+        print("resultado: ", numero_1 - numero_2)
+    elif operador == "*":
+        print("resultado: ", numero_1 * numero_2)
+    elif operador == "/":
+        if numero_2 == 0:
+            print("no se puede dividir un numero entre 0")
+        else:
+            print("resultado: ", numero_1 / numero_2)
+    elif operador == "//":
+        print("resultado: ", numero_1 // numero_2)
+    elif operador == "**":
+        print("resultado: ", numero_1 ** numero_2)
+    elif operador == "%":
+        print("resultado: ", numero_1 % numero_2)
+    else:
+        print("operador incorrecto : gracias por comprar Ecoin")
+    repetir = input("si desea volver a usar la calculadora presione s y si no presione cualquier otra letra")
     
-else:
-    print("valor invalido o operador faltante espere futuras actualizaciones")  
+    
+
+    
+
 
